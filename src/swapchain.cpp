ed9c6dc06b52183936ef250a4bb71144c0982620
cw228 <chriswacker228@gmail.com>
2026-03-15 22:41:45 -0700

    WIP

--- src/swapchain.cpp
+++ src/swapchain.cpp
@@ -1,117 +1,129 @@
-// #include "context.h"
-// #include "window.h"
-// #include "vulkan/vulkan_raii.hpp"
-//
-// vk::SurfaceFormatKHR chooseSwapSurfaceFormat(vk::raii::PhysicalDevice& physicalDevice, vk::raii::SurfaceKHR& surface) {
-//     std::vector<vk::SurfaceFormatKHR> availableFormats = physicalDevice.getSurfaceFormatsKHR(surface);
-//     for (const vk::SurfaceFormatKHR& format : availableFormats) {
-//         if (format.format == vk::Format::eB8G8R8A8Srgb && format.colorSpace == vk::ColorSpaceKHR::eSrgbNonlinear) {
-//             return format;
-//         }
-//     }
-//
-//     return availableFormats[0];
-// }
-//
-// vk::PresentModeKHR chooseSwapPresentMode(vk::raii::PhysicalDevice& physicalDevice, vk::raii::SurfaceKHR& surface) {
-//     std::vector<vk::PresentModeKHR> availablePresentModes = physicalDevice.getSurfacePresentModesKHR(surface);
-//     for (const vk::PresentModeKHR& presentMode : availablePresentModes) {
-//         if (presentMode == vk::PresentModeKHR::eMailbox) {
-//             return presentMode;
-//         }
-//     }
-//
-//     return vk::PresentModeKHR::eFifo;
-// }
-//
-// vk::Extent2D clampedExtent(vk::SurfaceCapabilitiesKHR& capabilities, int& width, int& height) {
-//     return {
-//         std::clamp<uint32_t>(width, capabilities.minImageExtent.width, capabilities.maxImageExtent.width),
-//         std::clamp<uint32_t>(height, capabilities.minImageExtent.height, capabilities.maxImageExtent.height),
-//     };
-// }
-//
-// vk::Extent2D chooseSwapExtent(Window& window, vk::SurfaceCapabilitiesKHR surfaceCapabilities) {
-//     if (surfaceCapabilities.currentExtent.width != std::numeric_limits<uint32_t>::max()) {
-//         return surfaceCapabilities.currentExtent;
-//     }
-//     int width, height;
-//     glfwGetFramebufferSize(window, &width, &height);
-//     return clampedExtent(surfaceCapabilities, width, height);
-// }
-//
-// vk::raii::SwapchainKHR createSwapchain(
-//     vk::raii::Device& device,
-//     vk::raii::SurfaceKHR& surface,
-//     vk::SurfaceFormatKHR format,
-//     vk::PresentModeKHR presentMode,
-//     vk::Extent2D extent,
-//     vk::SurfaceCapabilitiesKHR surfaceCapabilities,
-//     QueueFamilies queueFamilyIndices
-// ) {
-//     auto minImageCount = std::max(3u, surfaceCapabilities.minImageCount);
-//     if (surfaceCapabilities.maxImageCount > 0 && minImageCount > surfaceCapabilities.maxImageCount) {
-//         minImageCount = surfaceCapabilities.maxImageCount;
-//     }
-//
-//     vk::SwapchainCreateInfoKHR swapchainCreateInfo{
-//         .flags = vk::SwapchainCreateFlagsKHR(),
-//         .surface = *surface,
-//         .minImageCount = minImageCount,
-//         .imageFormat = format.format,
-//         .imageColorSpace = format.colorSpace,
-//         .imageExtent = extent,
-//         .imageArrayLayers = 1,
-//         .imageUsage = vk::ImageUsageFlagBits::eColorAttachment,
-//         .preTransform = surfaceCapabilities.currentTransform,
-//         .compositeAlpha = vk::CompositeAlphaFlagBitsKHR::eOpaque,
-//         .presentMode = presentMode,
-//         .clipped = true,
-//         .oldSwapchain = nullptr
-//     };
-//
-//     if (queueFamilyIndices.graphics != queueFamilyIndices.presentation) {
-//         uint32_t queueFamilyIndicesArray[] = { queueFamilyIndices.graphics, queueFamilyIndices.presentation };
-//         swapchainCreateInfo.imageSharingMode = vk::SharingMode::eConcurrent;
-//         swapchainCreateInfo.queueFamilyIndexCount = 2;
-//         swapchainCreateInfo.pQueueFamilyIndices = queueFamilyIndicesArray;
-//     } else {
-//         swapchainCreateInfo.imageSharingMode = vk::SharingMode::eExclusive;
-//     }
-//
-//     return vk::raii::SwapchainKHR(device, swapchainCreateInfo);
-// }
-//
-// void Renderer::createSwapchainImageViews() {
-//     swapchainImageViews.clear();
-//
-//     for (vk::Image image : swapchainImages) {
-//         swapchainImageViews.push_back(createImageView(image, swapchainImageFormat, vk::ImageAspectFlagBits::eColor, 1));
-//     }
-// }
+#include <limits>
+#include <algorithm>
 
-// struct Swapchain {
-//     vk::raii::SwapchainKHR swapchain = nullptr;
-//     std::vector<vk::raii::ImageView> imageViews;
-//     vk::SurfaceFormatKHR imageFormat;
-//     vk::PresentModeKHR presentMode;
-//     vk::Extent2D extent;
-//
-//     Swapchain(const VulkanContext& cxt, const Window& window, vk::raii::SwapchainKHR oldSwapchain = nullptr) {
-//         // Choose surface format
-//         vk::SurfaceFormatKHR swapchainImageFormat = chooseSwapSurfaceFormat(vContext.physicalDevice, vContext.surface);
-//
-//         // Choose present mode
-//         vk::PresentModeKHR swapchainPresentMode = chooseSwapPresentMode(vContext.physicalDevice, vContext.surface);
-//
-//         // Choose extent
-//         vk::SurfaceCapabilitiesKHR surfaceCapabilities = vContext.physicalDevice.getSurfaceCapabilitiesKHR(vContext.surface);
-//         vk::Extent2D swapchainExtent = chooseSwapExtent(window, surfaceCapabilities);
-//
-//
-//         vk::raii::SwapchainKHR swapchain = createSwapchain(
-//             vContext.device, vContext.surface, swapchainImageFormat, swapchainPresentMode, swapchainExtent, surfaceCapabilities, vContext.queueFamilies
-//         );
-//
-//     }
-// };
+#include "vulkan/vulkan_raii.hpp"
+
+#include "context.h"
+#include "window.h"
+
+vk::raii::ImageView createImageView(vk::Image image, vk::Format format, vk::ImageAspectFlagBits aspectMask, uint32_t mipLevels) {
+    vk::ImageViewCreateInfo createInfo{
+        .image = image,
+        .viewType = vk::ImageViewType::e2D,
+        .format = format,
+        .subresourceRange = {
+            .aspectMask = aspectMask,
+            .baseMipLevel = 0,
+            .levelCount = mipLevels,
+            .baseArrayLayer = 0,
+            .layerCount = 1
+        }
+    };
+
+    return vk::raii::ImageView(vContext.device, createInfo);
+}
+
+void createSwapchainImageViews() {
+    swapchainImageViews.clear();
+
+    for (vk::Image image : swapchainImages) {
+        swapchainImageViews.push_back(createImageView(image, swapchainImageFormat, vk::ImageAspectFlagBits::eColor, 1));
+    }
+}
+
+struct Swapchain {
+    vk::raii::SwapchainKHR swapchain = nullptr;
+    std::vector<vk::Image> images;
+    std::vector<vk::raii::ImageView> imageViews;
+    vk::SurfaceFormatKHR surfaceFormat;
+    vk::PresentModeKHR presentMode;
+    vk::Extent2D extent;
+
+    Swapchain(const VulkanContext& vContext, const Window& window, vk::raii::SwapchainKHR oldSwapchain = nullptr) {
+        std::vector<vk::SurfaceFormatKHR> formats = vContext.physicalDevice.getSurfaceFormatsKHR(vContext.surface);
+        std::vector<vk::PresentModeKHR> modes = vContext.physicalDevice.getSurfacePresentModesKHR(vContext.surface);
+        vk::SurfaceCapabilitiesKHR capabilities = vContext.physicalDevice.getSurfaceCapabilitiesKHR(vContext.surface);        
+
+        // Choose surface format
+        surfaceFormat = formats[0];
+
+        for (const vk::SurfaceFormatKHR& format : formats) {
+            if (format.format == vk::Format::eB8G8R8A8Srgb && format.colorSpace == vk::ColorSpaceKHR::eSrgbNonlinear) {
+                surfaceFormat = format;
+                break;
+            }
+        }
+
+        // Choose present mode
+        presentMode = vk::PresentModeKHR::eFifo;
+
+        for (const vk::PresentModeKHR& mode : modes) {
+            if (mode == vk::PresentModeKHR::eMailbox) {
+                presentMode = mode;
+                break;
+            }
+        }
+
+        // Choose extent
+        if (capabilities.currentExtent.width != std::numeric_limits<uint32_t>::max()) {
+            extent = capabilities.currentExtent;
+        } else {
+            int width, height;
+            glfwGetFramebufferSize(window, &width, &height);
+            extent = {
+                std::clamp<uint32_t>(width, capabilities.minImageExtent.width, capabilities.maxImageExtent.width),
+                std::clamp<uint32_t>(height, capabilities.minImageExtent.height, capabilities.maxImageExtent.height),
+            };
+        }
+
+        uint32_t minImageCount = std::max(3u, capabilities.minImageCount);
+        if (capabilities.maxImageCount > 0 && minImageCount > capabilities.maxImageCount) {
+            minImageCount = capabilities.maxImageCount;
+        }
+
+        // Create swapchain
+        vk::SwapchainCreateInfoKHR createInfo{
+            .flags = vk::SwapchainCreateFlagsKHR(),
+            .surface = *vContext.surface,
+            .minImageCount = minImageCount,
+            .imageFormat = surfaceFormat.format,
+            .imageColorSpace = surfaceFormat.colorSpace,
+            .imageExtent = extent,
+            .imageArrayLayers = 1,
+            .imageUsage = vk::ImageUsageFlagBits::eColorAttachment,
+            .preTransform = capabilities.currentTransform,
+            .compositeAlpha = vk::CompositeAlphaFlagBitsKHR::eOpaque,
+            .presentMode = presentMode,
+            .clipped = true,
+            .oldSwapchain = nullptr
+        };
+
+        if (vContext.queueFamilies.graphics != vContext.queueFamilies.presentation) {
+            uint32_t queueFamilies[] = { vContext.queueFamilies.graphics, vContext.queueFamilies.presentation };
+            createInfo.imageSharingMode = vk::SharingMode::eConcurrent;
+            createInfo.queueFamilyIndexCount = 2;
+            createInfo.pQueueFamilyIndices = queueFamilies;
+        } else {
+            createInfo.imageSharingMode = vk::SharingMode::eExclusive;
+        }
+
+        swapchain = vk::raii::SwapchainKHR(vContext.device, createInfo);
+
+        images = swapchain.getImages();
+
+        // Create image views
+        for (vk::Image& image : images) {
+
+        }
+
+        // Create multisample color image
+        // Create multisample depth image
+        // Create semaphores
+
+        // vk::raii::SwapchainKHR swapchain = createSwapchain(
+        //     vContext.device, vContext.surface, swapchainImageFormat, swapchainPresentMode, swapchainExtent, capabilities, vContext.queueFamilies
+        // );
+
+    }
+};
+
